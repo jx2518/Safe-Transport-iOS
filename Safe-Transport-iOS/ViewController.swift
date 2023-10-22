4212c70ad7fc961c52104d697b734d82483b1fec
Jingpu Xu <jx2518@columbia.edu>
2023-10-22 17:19:08 -0400

    Initial Commit

---
+++ Safe-Transport-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Safe-Transport-iOS
+//
+//  Created by  jx2518 on 10/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
